8dcae9cdd5d08cd27ebae87fa7a8aa39681e6b43
NoraBohm <nora.bohm@edu.huddinge.se>
2026-04-16 07:58:24 +0000

    Implimented start of client-side streaming code

---
+++ js/player.js
@@ -0,0 +1,55 @@
+const player = document.getElementById('player');
+const source = player.getElementsByTagName('source')[0];
+
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+/*const urlParams = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});*/
+const urlParams = new URLSearchParams(window.location.search);
+const video_id = urlParams.get("id");
+if (!Number.isInterger(video_id) || (0 > video_id)) {
+    throwError("No or invalid id");
+}
+
+function updateBufferProgress(bufferProgress) {
+    console.log(`Buffer progress: ${bufferProgress}`);
+}
+
+function throwError(message) {
+    console.log(`ERROR: ${message}`);
+}
+
+function loadVideo(id, res) {
+    /*
+    res {
+    0 = audio only (maybe impliment)
+    1 = 480p
+    2 = 720p
+    3 = 1080p
+    4 = 4k
+    }
+    */
+    source.src = `stream.php?id=${encodeURIComponent(id)}&res=${encodeURIComponent(res)}`;
+
+    player.load();
+
+    if (!urlParams.has("no-autoplay")) {
+        // Impliment autoplay
+        player.addEventListener('loadeddata', function() {
+            player.play().catch(e => {
+                console.log(`Autoplay failed: {e}`);
+            });
+        }, { once: true });
+    }
+}
+
+// Video player analytics
+player.addEventListener('progress', function() {
+    const buffered = this.buffered;
+    if (buffered.length > 0) {
+        const endOfBuffer = buffered.end(buffered.length - 1);
+        const duration = this.duration;
+        const bufferProgress = endOfBuffer / duration;
+        updateBufferProgress(bufferProgress);
+    }  
+});
---
+++ video.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Player</title>
+    <script src="/js/player.js"></script>
+</head>
+<body>
+    <noscript>You need to enable Javascript to use this service.</noscript>
+    <video id="player" controls preload="metadata">
+        <source src="" type="video/webm">
+    </video>
+
+    <menu>
+        Resolution selector here or something
+    </menu>
+</body>
+</html>
